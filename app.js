3b59477fd610ee16aa0299fe1bcd6c92b12d2f07
Camila Fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-07-16 19:30:01 -0400

    login facebook y google

--- app.js
+++ app.js
@@ -34,6 +34,54 @@ window.login = () => {
     let errorMessage = error.message;
 
 };
+window.facebookLogin = () => {
+
+  let provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      let token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    let user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    // The email of the user's account used.
+    let email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    let credential = error.credential;
+    // ...
+  });
+};
+
+window.googleLogin = () => {
+  let provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithRedirect(provider);
+  firebase.auth().getRedirectResult().then((result) => {
+    
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      let token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    let user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    // The email of the user's account used.
+    let email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    let credential = error.credential;
+    // ...
+  });
+};
+
 const logOut = () => {
   firebase.auth().signOut();
 };
