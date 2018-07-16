22a5fe58c4a360de2f4a22f368312b33c7f6616a
Camila Fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-07-15 22:12:36 -0400

    login

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-window.login = () => {
-  
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    document.getElementById("user").style.display = "block";
+    document.getElementById("login").style.display = "none";
+  } else {
+    document.getElementById("user").style.display = "none";
+    document.getElementById("login").style.display = "block";
+  }
+});
 
+indow.login = () => {
+  let login = document.getElementById("staticEmail").value;
+  let password = document.getElementById("inputPassword").value;
 };
